abff0ce9db51003efd34c0af5d71d952bb94f519
shibamoto <silverbirder@gmail.com>
2020-02-18 07:20:06 +0900

    Update template file

--- README.md
+++ README.md
@@ -8,5 +8,12 @@ $ npm install -D
 $ npm run login
 $ npm run init
 $ rm appscript.json
-$ 
+$ sed -e 's/}/, "rootDir": ".\/dist"}/g' .clasp.json > t && mv t .clasp.json
+```
+
+## Dev
+```
+$ npm run build
+$ npm run push
+$ npm run test
 ```
--- __tests__/src/index.test.ts
+++ __tests__/src/index.test.ts
@@ -1,5 +1,14 @@
-describe('', () => {
-   test('', () => {
-      expect(1).toBe(1);
+import {greet} from "../../src";
+
+describe('Method: greet', () => {
+   test('Assert: call console.log', () => {
+      // Arrange
+      jest.spyOn(console, 'log');
+
+      // Act
+      greet();
+
+      // Assert
+      expect(console.log).toBeCalled();
    });
 });
